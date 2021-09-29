8d10405a4e216f23b4dd136b00e1dacb7c245906
Sakshi <sakshishetty1231997@gmail.com>
2021-09-29 18:56:18 +0530

    Added: Code and test case to park the car in the parking lot

---
+++ src/main/java/parkinglot/ParkingLotSystem.java
@@ -0,0 +1,8 @@
+package parkinglot;
+
+public class ParkingLotSystem {
+
+    public boolean park(Object vehicle) {
+        return true;
+    }
+}
---
+++ src/test/java/parkinglot/ParkingLotTest.java
@@ -0,0 +1,14 @@
+package parkinglot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ParkingLotTest {
+
+    @Test
+    public void givenAVehicle_WhenParked_ShouldReturnTrue(){
+        ParkingLotSystem parkinglotsystem = new ParkingLotSystem();
+        boolean isParked = parkinglotsystem.park(new Object());
+        Assertions.assertTrue(isParked);
+    }
+}
