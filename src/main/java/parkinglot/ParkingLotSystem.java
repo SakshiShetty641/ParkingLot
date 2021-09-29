ccba9e5cfc47df4621207d4d741517d7cfd6f320
Sakshi <sakshishetty1231997@gmail.com>
2021-09-29 22:46:07 +0530

    Added: Code and test case to unpark the car in the parking lot

--- src/main/java/parkinglot/ParkingLotSystem.java
+++ src/main/java/parkinglot/ParkingLotSystem.java
@@ -1,8 +1,29 @@
 package parkinglot;
 
+/**
+ * Purpose - To simulate a Parking Lot System
+ *
+ * @author - Sakshi Shetty
+ * @version - 8.0
+ * @since - 2021-09-29
+ */
 public class ParkingLotSystem {
 
+    private Object vehicle;
+
     public boolean park(Object vehicle) {
-        return true;
+        if (this.vehicle == null) {
+            this.vehicle = vehicle;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean unPark(Object vehicle) {
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
