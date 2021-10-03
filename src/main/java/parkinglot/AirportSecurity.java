39dbe73c6dcf0113296c085d4ad4a245eb5cdef1
Sakshi <sakshishetty1231997@gmail.com>
2021-10-03 21:37:21 +0530

    Refactored: To inform the observers when parking space is available again

--- src/main/java/parkinglot/AirportSecurity.java
+++ src/main/java/parkinglot/AirportSecurity.java
@@ -3,8 +3,14 @@ package parkinglot;
 public class AirportSecurity implements ParkingLotObserver {
     private boolean isFullCapacity;
 
+    @Override
     public void capacityIsFull() {
         isFullCapacity = true;
+    }
+
+    @Override
+    public void capacityIsAvailable() {
+        isFullCapacity = false;
     }
 
     public boolean isCapacityFull() {
--- src/main/java/parkinglot/ParkingLotOwner.java
+++ src/main/java/parkinglot/ParkingLotOwner.java
@@ -3,8 +3,14 @@ package parkinglot;
 public class ParkingLotOwner implements ParkingLotObserver {
     private boolean isFullCapacity;
 
+    @Override
     public void capacityIsFull() {
         isFullCapacity = true;
+    }
+
+    @Override
+    public void capacityIsAvailable() {
+        isFullCapacity = false;
     }
 
     public boolean isCapacityFull() {
